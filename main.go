264ea184cee8faad4dc5c191c0ea3ab84d8c7fa0
Anton Kunaev <kunaevanton25@gmail.com>
2024-01-29 19:57:01 +0300

    Implement function loadTemplates

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 )
 
@@ -13,7 +14,17 @@ var responses = make([]*Rsvp, 0, 10)
 var templates = make(map[string]*template.Template, 3)
 
 func loadTemplates() {
-	//	ToDo - load templates here
+	templateNames := [5]string{"welcome", "form", "thanks", "sorry", "list"}
+
+	for index, name := range templateNames {
+		t, err := template.ParseFiles("layout.html", name+".html")
+		if err == nil {
+			templates[name] = t
+			fmt.Println("Loaded template", index, name)
+		} else {
+			panic(err)
+		}
+	}
 }
 
 func main() {
